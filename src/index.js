0b852278d5aa11d6a8e0a62f68082f4eab4f39c0
Marlon <marlon.bobi@hotmail.com>
2019-07-27 17:40:24 -0300

    Init project

---
+++ src/index.js
@@ -0,0 +1,298 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+// const name = {Name1: "Marlon", Name2:"Jose"};
+
+
+// function tick(){
+//     const element = (
+//         <div>
+//             <h1>Hello, world!</h1>
+//             <h2>It is {new Date().toLocaleTimeString()}.</h2>
+//         </div>
+//     );
+//     ReactDOM.render(
+
+//         element,
+//         document.getElementById('root')
+//       );
+    
+// }
+// setInterval(tick, 1000)
+
+// function Welcome(props){
+//     return<h1>Hello, {props.name}</h1>;
+// }
+
+// const element = <Welcome name="Marlon"/>
+
+// ReactDOM.render(
+
+//             element,
+//             document.getElementById('root')
+//           );
+ 
+// function UserInfo(props){
+//     return(
+//         <div className="MarlonBlz!">
+//         <div className="MarlonYeah!"></div>
+//         </div>
+
+//     );
+// }
+
+
+
+
+// function Comment(props){
+//     return(
+//         <div className="Comment-text">
+//             {props.text}
+//         </div>
+
+//     );
+// }
+
+// const comment ={
+
+//     text: 'Testando!',
+// }
+
+
+// ReactDOM.render(
+
+//     <Comment
+//     text={comment.text}
+//     />,
+//     document.getElementById('root')
+// );
+
+
+// class Clock extends React.Component{
+//     constructor(props){
+//         super(props);
+//         this.state = {date: new Date()};
+//     }
+
+//     componentDidMount(){
+//         this.timerID = setInterval(
+//             () => this.tick(),
+//             1000
+//         );
+
+//     }
+//     componentWillUnmount(){
+//         clearInterval(this.timerID);
+
+//     }
+
+//     tick(){
+//         this.setState({
+//             date: new Date()
+//         });
+//     }
+    
+//     render(){
+//     return(
+//         <div>
+//             <h1>Hello, world!</h1>
+//             <h2>Its {this.state.date.toLocaleTimeString()}.</h2>
+//         </div>
+//     );
+// }
+// }
+
+
+//     ReactDOM.render(
+//         <Clock />,
+//         document.getElementById('root')
+//     );
+
+    
+// // setInterval(tick, 1000);
+
+{/* <button onClick={activateLasers}>
+    Ativar lasers
+</button> */}
+
+
+// function ActionLink(){
+//     function handleClick(e){
+//         e.preventDefault();
+//         console.log('O link foi clicado.');
+
+//     }
+// }
+// return (
+//     <a href='#' onClick={handleClick}>
+//         Clique aqui
+//     </a>
+// )
+
+// class Toggle extends React.Component{
+//     constructor(props){
+//         super(props);
+//         this.state = {isToggleOn: true};
+
+//         //Aqui utilizamos o `bind` para que o `this` funcione dentro da nossa callback.
+//         this.handleClick = this.handleClick.bind(this);
+//     }
+
+//     handleClick() {
+//         this.setState(state => ({
+//             isToggleOn: !state.isToggleOn
+//         }));
+//     }
+//     render(){
+//         return (
+//             <button onClick={this.handleClick}>
+//                 {this.state.isToggleOn ? 'ON' : 'OFF'}
+//             </button>
+//         );
+//     }
+// }
+
+
+// class LogButton extends React.Component{
+
+
+
+
+//     render(){
+//         <button onClick={(e) => this.deleteRow(id, e)}>Deletar linha</button>
+//         }
+// }
+
+// class LoginControl extends React.Component{
+//     constructor(props){
+//         super(props);
+//         this.handleLoginClick = this.handleLoginClick.bind(this);
+//         this.handleLogoutClick = this.handleLogoutClick.bind(this);
+//         this.state = {isLoggedIn: false};
+//     }
+
+//     handleLoginClick(){
+//         this.setState({isLoggedIn: true});
+//     }
+
+//     handleLogoutClick(){
+//         this.setState({isLoggedIn: false});
+//     }
+
+//     render(){
+//         const isLoggedIn = this.state.isLoggedIn;
+//         let button;
+
+//         if(isLoggedIn){
+//             button = <LogoutButton onClick={this.handleLogoutClick}/>;
+//         }else{
+//             button = <LoginButton onClick={this.handleLoginClick} />;
+//         }
+
+//         return (
+//             <div>
+//                 <Greeting isLoggedIn={isLoggedIn} />
+//                 {button}
+//             </div>
+
+//         );
+//     }
+// }
+
+
+
+// function UserGreeting(props){
+//     return <h1>Welcome back!</h1>;
+// }
+// function GuestGreeting(props){
+//     return <h1>Please sign up.</h1>
+// }
+
+// function LoginButton(props){
+//     return (
+//         <button onClick={props.onClick}>
+//             Login
+//         </button>
+//     );
+// }
+// function LogoutButton(props){
+//     return(
+//         <button onClick={props.onClick}>
+//             Logout
+//         </button>
+//     );
+// }
+
+
+
+// function Greeting(props){
+    
+//     const isLoggedIn = props.isLoggedIn;
+//     if(isLoggedIn){
+//         return <UserGreeting/>;
+//     }
+//     return <GuestGreeting/>;
+// }
+
+// ReactDOM.render(
+    
+//     <LoginControl/>,
+//     document.getElementById('root')
+// )
+
+// function Mailbox(props){
+//     const unreadMessages = props.unreadMessages;
+//     return(
+
+//         <div>
+//             <h1>Hello!</h1>
+//             {unreadMessages.length > 0 && 
+//             <h2>
+//                 You have {unreadMessages.length} unread messages.
+//             </h2>
+            
+//             }
+//         </div>
+//     );
+// }
+
+// const messages = ['React', 'Re: React', 'Re:Re: React'];
+// ReactDOM.render(
+
+//     <Mailbox unreadMessages={messages}/>,
+//     document.getElementById('root')
+// )
+
+// function NumberList(props) {
+//     const numbers = props.numbers;
+//     const listItems = numbers.map((number) =>
+//       <li>{number}</li>
+//     );
+//     return (
+//       <ul>{listItems}</ul>
+//     );
+//   }
+  
+//   const numbers = [1, 2, 3, 4, 5];
+//   ReactDOM.render(
+//     <NumberList numbers={numbers} />,
+//     document.getElementById('root')
+//   );
+
+// function NumberList(props) {
+
+// const listItems = numbers.map((number) =>
+//   <li key={number.toString()}>
+//     {number}
+//   </li>
+// );
+//     return (
+//       <ul>{listItems}</ul>
+//     );
+//   }
+//   const numbers = [1, 2, 3, 4, 5];
+//   ReactDOM.render(
+//     <NumberList numbers={numbers} />,
+//     document.getElementById('root')
+//   );
