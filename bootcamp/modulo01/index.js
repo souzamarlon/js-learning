fb7e09d4530f9d1ebd2bb43553b0c55e20b3807a
Marlon <marlon.bobi@hotmail.com>
2019-08-19 22:52:36 -0300

    Finishing

--- bootcamp/modulo01/index.js
+++ bootcamp/modulo01/index.js
@@ -3,8 +3,27 @@ const express = require('express');
 const server = express();
 
 
-server.get('/teste', (req, res) => {
-    return res.send('Hello World!');
+server.get('/users/:id', (req, res) => {
+    const {id} = req.params;
+
+    return res.json({message: `Usuário: ${id}` })
 })
 
 server.listen(3000);
+
+
+//Route params
+// server.get('/users/:id', (req, res) => {
+//     const id = req.params.id;
+
+//     return res.json({message: `Usuário: ${id}` })
+// })
+
+
+
+// Query params
+// server.get('/teste', (req, res) => {
+//     const nome = req.query.nome;
+
+//     return res.json({message: `Hello ${nome}` })
+// })
