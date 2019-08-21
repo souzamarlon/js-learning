a46cdd627e9a9d4cc7ad111e95b93dcadaedb288
Marlon <marlon.bobi@hotmail.com>
2019-08-20 22:03:08 -0300

    Class_20_08_2019

--- bootcamp/modulo01/index.js
+++ bootcamp/modulo01/index.js
@@ -2,11 +2,47 @@ const express = require('express');
 
 const server = express();
 
+server.use(express.json());
 
-server.get('/users/:id', (req, res) => {
-    const {id} = req.params;
+const users = ['Diego', 'Claudio2', 'Victor'];
 
-    return res.json({message: `Usuário: ${id}` })
+server.get('/users', (req, res) => {
+    return res.json(users)
+
+})
+
+
+server.get('/users/:index', (req, res) => {
+    const {index} = req.params;
+
+    return res.json(users[index]);
+})
+
+
+server.post('/users', (req, res) =>{
+    const {name} = req.body;
+
+    users.push(name);
+
+    return res.json(users);
+})
+
+server.put('/users/:index', (req, res) => {
+    const { index } = req.params;
+    const { name } = req.body;
+
+    users[index] = name;
+
+    return res.json(users);
+
+})
+
+server.delete('/users/:index', (req, res) => {
+    const { index } = req.params;
+
+    users.splice(index, 1);
+
+    return res.send();
 })
 
 server.listen(3000);
