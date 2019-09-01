62117a164b3c55b8378f36476d963969d12ab420
Marlon <marlon.bobi@hotmail.com>
2019-09-01 19:53:51 -0300

    Modulo02-Configurando fila com Redis

--- bootcamp/modulo02/src/app/controllers/AppointmentController.js
+++ bootcamp/modulo02/src/app/controllers/AppointmentController.js
@@ -6,7 +6,9 @@ import User from '../models/User';
 import File from '../models/File';
 import Notification from '../schemas/Notification';
 
-import Mail from '../../lib/Mail';
+import CancellationMail from '../jobs/CancellationMail';
+
+import Queue from '../../lib/Queue';
 
 class AppointmentController {
   async index(req, res) {
@@ -127,6 +129,11 @@ class AppointmentController {
           as: 'provider',
           attributes: ['name', 'email'],
         },
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name'],
+        },
       ],
     });
 
@@ -148,10 +155,8 @@ class AppointmentController {
 
     await appointment.save();
 
-    await Mail.sendMail({
-      to: `${appointment.provider.name} <${appointment.provider.email}`,
-      subject: 'Agendamento cancelado',
-      text: 'Você tem um novo cancelamento',
+    await Queue.add(CancellationMail.key, {
+      appointment,
     });
 
     return res.json(appointment);
