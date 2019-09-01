234763fe788a6556fc316207d4c9ea0082d6a303
Marlon <marlon.bobi@hotmail.com>
2019-09-01 17:00:00 -0300

    Modulo02-Configurando Nodemailer

---
+++ bootcamp/modulo02/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '44d28fdc3cea76',
+    pass: '9001548e0283ce',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
