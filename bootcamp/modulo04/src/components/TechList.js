01ca618cfada12c0b0790d4a19f083546c21476c
Marlon <marlon.bobi@hotmail.com>
2019-09-04 22:37:21 -0300

    Modulo04-Removendo itens do estado

--- bootcamp/modulo04/src/components/TechList.js
+++ bootcamp/modulo04/src/components/TechList.js
@@ -22,11 +22,21 @@ handleSubmit = e =>{
  newTech:''
 })
 }
+
+handleDelete = (tech) =>{
+
+    this.setState({ techs: this.state.techs.filter(t => t !== tech)})
+}
     render(){
         return(
             <form onSubmit={this.handleSubmit}>
             <ul>
-              {this.state.techs.map(tech => <li key={tech}>{tech}</li>)}
+              {this.state.techs.map(tech => (
+              <li key={tech}>
+              {tech}
+              <button onClick={() => this.handleDelete(tech)} type="button">Remover</button>
+              </li>
+              ))}
             </ul>
             <input 
             type="text" 
