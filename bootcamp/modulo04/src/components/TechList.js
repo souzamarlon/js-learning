8a1609f6d1f931f1e4e2c1cd0480745547937a6f
Marlon <marlon.bobi@hotmail.com>
2019-09-04 21:36:29 -0300

    Modulo04-Estado & Imutabilidade (Very important!)

--- bootcamp/modulo04/src/components/TechList.js
+++ bootcamp/modulo04/src/components/TechList.js
@@ -4,18 +4,37 @@ import React, { Component } from 'react';
 class TechList extends Component{
 
 state = {
-    tech:[
+    newTech:'',
+    techs:[
         'Node.js',
         'ReactJS',
         'React Native'
     ]
-};    
+}; 
+
+handleInputChange = e =>{
+    this.setState({ newTech: e.target.value})
+}
+handleSubmit = e =>{
+ e.preventDefault();
+
+ this.setState({ techs: [ ... this.state.techs, this.state.newTech],
+ newTech:''
+})
+}
     render(){
-        console.log(this.state)
         return(
+            <form onSubmit={this.handleSubmit}>
             <ul>
-                <li>Node</li>
+              {this.state.techs.map(tech => <li key={tech}>{tech}</li>)}
             </ul>
+            <input 
+            type="text" 
+            onChange={this.handleInputChange}
+            value={this.state.newTech}
+            />
+            <button type="submit">Enviar</button>
+            </form>
         );
     }
 }
