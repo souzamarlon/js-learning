8b78ac6c8225eb4c7c34f876104891993908b7cc
Marlon <marlon.bobi@hotmail.com>
2019-09-07 23:39:02 -0300

    Modulo02-Desafio03 07_09_UPDATE

--- bootcamp/modulo_desafio_nodejs/src/app/controllers/MeetupController.js
+++ bootcamp/modulo_desafio_nodejs/src/app/controllers/MeetupController.js
@@ -1,6 +1,5 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore, format, subHours } from 'date-fns';
-import pt from 'date-fns/locale/pt';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 import Meetup from '../models/Meetup';
 // import File from '../models/File';
 
@@ -22,6 +21,7 @@ class MeetupController {
       title: Yup.string().required(),
       description: Yup.string().required(),
       location: Yup.string().required(),
+      image: Yup.number().required(),
       date: Yup.date().required(),
     });
 
@@ -31,7 +31,7 @@ class MeetupController {
           'Please make sure all required fields are filled out correctly. Validation fails!',
       });
     }
-    const { title, description, location, date } = req.body;
+    const { title, description, location, image, date } = req.body;
 
     /**
      * Check if the inserted date is outdated.
@@ -48,21 +48,51 @@ class MeetupController {
       title,
       description,
       location,
+      image,
       date,
     });
     return res.json(meetup);
   }
 
   async update(req, res) {
-    const { id, title, description, location, date } = req.body;
-    const meet_req = await Meetup.findByPk(id);
+    const { id, title, description, location, image, date } = req.body;
+    const meet_update = await Meetup.findByPk(id);
 
-    const meetup = await meet_req.update({
+    if (meet_update.user_id !== req.userId) {
+      return res.status(401).json({
+        error: 'You do not have authorization to update this Meetup.',
+      });
+    }
+    /** TODO - Actual date (It is not Brazilian time ) */
+    // const actualDate = new Date();
+    // console.log(actualDate);
+
+    const hourStart = startOfHour(meet_update.date);
+
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'Past dates are not permitted!' });
+    }
+
+    const meetup = await meet_update.update({
       title,
       description,
       location,
+      image,
       date,
     });
+    return res.json(meetup);
+  }
+
+  async delete(req, res) {
+    const meet_remove = await Meetup.findByPk(req.params.id);
+
+    if (meet_remove.user_id !== req.userId) {
+      return res.status(401).json({
+        error: 'You do not have authorization to update this Meetup.',
+      });
+    }
+
+    const meetup = await meet_remove.destroy();
     return res.json(meetup);
   }
 }
--- bootcamp/modulo_desafio_nodejs/src/routes.js
+++ bootcamp/modulo_desafio_nodejs/src/routes.js
@@ -21,5 +21,6 @@ routes.post('/files', upload.single('file'), FileController.store);
 routes.post('/meetups', MeetupController.store);
 routes.get('/meetups', MeetupController.index);
 routes.put('/meetups', MeetupController.update);
+routes.delete('/meetups/:id', MeetupController.delete);
 
 export default routes;
