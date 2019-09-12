0e8ac09499ac07fca125aa310bf4c047ae57ebe1
Marlon <marlon.bobi@hotmail.com>
2019-09-11 21:10:07 -0300

    Modulo07-Estilização da Home

--- bootcamp/modulo07/src/pages/Home/index.js
+++ bootcamp/modulo07/src/pages/Home/index.js
@@ -1,7 +1,101 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { MdAddShoppingCart } from 'react-icons/md';
+import { ProductList } from './styles';
 
 export default function Home() {
-    return <h1>Home</h1>;
+    return (
+        <ProductList>
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/tenis-vibe-crew-masculino/26/411-3605-026/411-3605-026_detalhe2.jpg?ims=326x"
+                    alt="Tênis"
+                />
+                <strong>Tênis muito legal!</strong>
+                <span>R$129,90</span>
+
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" />3
+                    </div>
+                    <span>ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/tenis-vibe-crew-masculino/26/411-3605-026/411-3605-026_detalhe2.jpg?ims=326x"
+                    alt="Tênis"
+                />
+                <strong>Tênis muito legal!</strong>
+                <span>R$129,90</span>
+
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" />3
+                    </div>
+                    <span>ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/tenis-vibe-crew-masculino/26/411-3605-026/411-3605-026_detalhe2.jpg?ims=326x"
+                    alt="Tênis"
+                />
+                <strong>Tênis muito legal!</strong>
+                <span>R$129,90</span>
+
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" />3
+                    </div>
+                    <span>ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/tenis-vibe-crew-masculino/26/411-3605-026/411-3605-026_detalhe2.jpg?ims=326x"
+                    alt="Tênis"
+                />
+                <strong>Tênis muito legal!</strong>
+                <span>R$129,90</span>
+
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" />3
+                    </div>
+                    <span>ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/tenis-vibe-crew-masculino/26/411-3605-026/411-3605-026_detalhe2.jpg?ims=326x"
+                    alt="Tênis"
+                />
+                <strong>Tênis muito legal!</strong>
+                <span>R$129,90</span>
+
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" />3
+                    </div>
+                    <span>ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/tenis-vibe-crew-masculino/26/411-3605-026/411-3605-026_detalhe2.jpg?ims=326x"
+                    alt="Tênis"
+                />
+                <strong>Tênis muito legal!</strong>
+                <span>R$129,90</span>
+
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" />3
+                    </div>
+                    <span>ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+        </ProductList>
+    );
 }
