e38f599259cba9604661cf5ce13c0625205536f9
Marlon <marlon.bobi@hotmail.com>
2019-09-12 22:06:37 -0300

    Modulo07-Configurando o Redux

--- bootcamp/modulo07/src/pages/Home/index.js
+++ bootcamp/modulo07/src/pages/Home/index.js
@@ -1,101 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import { MdAddShoppingCart } from 'react-icons/md';
+import { formatPrice } from '../../util/format';
+import api from '../../services/api';
 import { ProductList } from './styles';
 
-export default function Home() {
-    return (
-        <ProductList>
-            <li>
-                <img
-                    src="https://static.netshoes.com.br/produtos/tenis-vibe-crew-masculino/26/411-3605-026/411-3605-026_detalhe2.jpg?ims=326x"
-                    alt="Tênis"
-                />
-                <strong>Tênis muito legal!</strong>
-                <span>R$129,90</span>
+export default class Home extends Component {
+    state = {
+        products: [],
+    };
 
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#FFF" />3
-                    </div>
-                    <span>ADICIONAR AO CARRINHO</span>
-                </button>
-            </li>
-            <li>
-                <img
-                    src="https://static.netshoes.com.br/produtos/tenis-vibe-crew-masculino/26/411-3605-026/411-3605-026_detalhe2.jpg?ims=326x"
-                    alt="Tênis"
-                />
-                <strong>Tênis muito legal!</strong>
-                <span>R$129,90</span>
+    async componentDidMount() {
+        const response = await api.get('products');
+        const data = response.data.map(product => ({
+            ...product,
+            priceFormatted: formatPrice(product.price),
+        }));
+        this.setState({ products: data });
+    }
 
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#FFF" />3
-                    </div>
-                    <span>ADICIONAR AO CARRINHO</span>
-                </button>
-            </li>
-            <li>
-                <img
-                    src="https://static.netshoes.com.br/produtos/tenis-vibe-crew-masculino/26/411-3605-026/411-3605-026_detalhe2.jpg?ims=326x"
-                    alt="Tênis"
-                />
-                <strong>Tênis muito legal!</strong>
-                <span>R$129,90</span>
+    render() {
+        const { products } = this.state;
+        return (
+            <ProductList>
+                {products.map(product => (
+                    <li key={product.id}>
+                        <img src={product.image} alt={product.title} />
+                        <strong>{product.title}</strong>
+                        <span>{product.priceFormatted}</span>
 
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#FFF" />3
-                    </div>
-                    <span>ADICIONAR AO CARRINHO</span>
-                </button>
-            </li>
-            <li>
-                <img
-                    src="https://static.netshoes.com.br/produtos/tenis-vibe-crew-masculino/26/411-3605-026/411-3605-026_detalhe2.jpg?ims=326x"
-                    alt="Tênis"
-                />
-                <strong>Tênis muito legal!</strong>
-                <span>R$129,90</span>
-
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#FFF" />3
-                    </div>
-                    <span>ADICIONAR AO CARRINHO</span>
-                </button>
-            </li>
-            <li>
-                <img
-                    src="https://static.netshoes.com.br/produtos/tenis-vibe-crew-masculino/26/411-3605-026/411-3605-026_detalhe2.jpg?ims=326x"
-                    alt="Tênis"
-                />
-                <strong>Tênis muito legal!</strong>
-                <span>R$129,90</span>
-
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#FFF" />3
-                    </div>
-                    <span>ADICIONAR AO CARRINHO</span>
-                </button>
-            </li>
-            <li>
-                <img
-                    src="https://static.netshoes.com.br/produtos/tenis-vibe-crew-masculino/26/411-3605-026/411-3605-026_detalhe2.jpg?ims=326x"
-                    alt="Tênis"
-                />
-                <strong>Tênis muito legal!</strong>
-                <span>R$129,90</span>
-
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#FFF" />3
-                    </div>
-                    <span>ADICIONAR AO CARRINHO</span>
-                </button>
-            </li>
-        </ProductList>
-    );
+                        <button type="button">
+                            <div>
+                                <MdAddShoppingCart size={16} color="#FFF" />3
+                            </div>
+                            <span>ADICIONAR AO CARRINHO</span>
+                        </button>
+                    </li>
+                ))}
+            </ProductList>
+        );
+    }
 }
