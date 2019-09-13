e1fbe1a9c37752e95394e37eb6e9859182708ca4
Marlon <marlon.bobi@hotmail.com>
2019-09-12 21:05:06 -0300

    Modulo04-Update_12_09_2019

--- bootcamp/modulo04_desafio_react/src/components/Header.js
+++ bootcamp/modulo04_desafio_react/src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Header() {
     return (
         <header>
-            <h1>Header</h1>
+            <h1>Facebook</h1>
             <h2>Login</h2>
         </header>
     );
