a621c2fdf3da641ca286fa0dff94b1336dc303eb
Marlon <marlon.bobi@hotmail.com>
2019-09-13 22:28:43 -0300

    Modulo07-Adicionando ao carrinho

--- bootcamp/modulo07/src/pages/Home/index.js
+++ bootcamp/modulo07/src/pages/Home/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import { MdAddShoppingCart } from 'react-icons/md';
 import { formatPrice } from '../../util/format';
 import api from '../../services/api';
 import { ProductList } from './styles';
 
-export default class Home extends Component {
+class Home extends Component {
     state = {
         products: [],
     };
@@ -19,6 +20,14 @@ export default class Home extends Component {
         this.setState({ products: data });
     }
 
+    handleAddProduct = product => {
+        const { dispatch } = this.props;
+        dispatch({
+            type: 'ADD_TO_CART',
+            product,
+        });
+    };
+
     render() {
         const { products } = this.state;
         return (
@@ -29,7 +38,10 @@ export default class Home extends Component {
                         <strong>{product.title}</strong>
                         <span>{product.priceFormatted}</span>
 
-                        <button type="button">
+                        <button
+                            type="button"
+                            onClick={() => this.handleAddProduct(product)}
+                        >
                             <div>
                                 <MdAddShoppingCart size={16} color="#FFF" />3
                             </div>
@@ -41,3 +53,4 @@ export default class Home extends Component {
         );
     }
 }
+export default connect()(Home);
