bc7def214ec8316d4b01ff3d6f7e2f7def142ccd
Marlon Souza <42625569+souzamarlon@users.noreply.github.com>
2019-09-18 08:22:17 -0300

    Modulo07-Refatorando as actions

--- bootcamp/modulo07/src/pages/Cart/index.js
+++ bootcamp/modulo07/src/pages/Cart/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
 import {
     MdRemoveCircleOutline,
     MdAddCircleOutline,
     MdDelete,
 } from 'react-icons/md';
 
+import * as CartActions from '../../store/modules/cart/actions';
+
 import { Container, ProductTable, Total } from './styles';
 
-function Cart({ cart, dispatch }) {
+function Cart({ cart, removeFromCart }) {
     return (
         <Container>
             <ProductTable>
@@ -58,12 +62,7 @@ function Cart({ cart, dispatch }) {
                             <td>
                                 <button
                                     type="button"
-                                    onClick={() =>
-                                        dispatch({
-                                            type: 'REMOVE_FROM_CART',
-                                            id: product.id,
-                                        })
-                                    }
+                                    onClick={() => removeFromCart(product.id)}
                                 >
                                     <MdDelete size={20} color="#7159c1" />
                                 </button>
@@ -87,4 +86,9 @@ const mapStateToProps = state => ({
     cart: state.cart,
 });
 
-export default connect(mapStateToProps)(Cart);
+const mapDispatchToProps = dispatch =>
+    bindActionCreators(CartActions, dispatch);
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Cart);
