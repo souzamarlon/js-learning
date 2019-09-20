a63753d92f3a7f55a1bc14a54b125300db2b9736
Marlon <marlon.bobi@hotmail.com>
2019-09-20 16:12:07 -0300

    Modulo07-Configurando Redux Saga

--- bootcamp/modulo07/src/pages/Home/index.js
+++ bootcamp/modulo07/src/pages/Home/index.js
@@ -23,9 +23,9 @@ class Home extends Component {
         this.setState({ products: data });
     }
 
-    handleAddProduct = product => {
-        const { addToCart } = this.props;
-        addToCart(product);
+    handleAddProduct = id => {
+        const { addToCartRequest } = this.props;
+        addToCartRequest(id);
     };
 
     render() {
@@ -41,7 +41,7 @@ class Home extends Component {
 
                         <button
                             type="button"
-                            onClick={() => this.handleAddProduct(product)}
+                            onClick={() => this.handleAddProduct(product.id)}
                         >
                             <div>
                                 <MdAddShoppingCart size={16} color="#FFF" />
