9cec8d419bf8f23472eb15c3688fcd75b3f3ad72
Marlon Souza <42625569+souzamarlon@users.noreply.github.com>
2019-09-27 08:34:57 -0300

    Modulo09-Atualizando perfil

--- bootcamp/modulo09/src/store/modules/user/reducer.js
+++ bootcamp/modulo09/src/store/modules/user/reducer.js
@@ -4,12 +4,17 @@ const INITIAL_STATE = {
     profile: null,
 };
 export default function auth(state = INITIAL_STATE, action) {
-    switch (action.type) {
-        case '@auth/SIGN_IN_SUCCESS':
-            return produce(state, draft => {
+    return produce(state, draft => {
+        switch (action.type) {
+            case '@auth/SIGN_IN_SUCCESS': {
                 draft.profile = action.payload.user;
-            });
-        default:
-            return state;
-    }
+                break;
+            }
+            case '@auth/UPDATE_PROFILE_SUCESS': {
+                draft.profile = action.payload.profile;
+                break;
+            }
+            default:
+        }
+    });
 }
