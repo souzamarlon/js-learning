393a5b4c24a50e5a3f4ae173eb3ecbaeaa1bc1bd
Marlon <marlon.bobi@hotmail.com>
2019-09-28 21:05:02 -0300

    Modulo09-Logout da aplicação

--- bootcamp/modulo09/src/pages/Profile/index.js
+++ bootcamp/modulo09/src/pages/Profile/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Form, Input } from '@rocketseat/unform';
 
+import { signOut } from '~/store/modules/auth/actions';
+
 import { updateProfileRequest } from '~/store/modules/user/actions';
 
 import AvatarInput from './AvatarInput';
@@ -17,6 +19,11 @@ export default function Profile() {
         dispatch(updateProfileRequest(data));
         // console.tron.log(data);
     }
+
+    function handleSignOut() {
+        dispatch(signOut());
+    }
+
     return (
         <Container>
             <Form initialData={profile} onSubmit={handleSubmit}>
@@ -45,7 +52,9 @@ export default function Profile() {
                 />
                 <button type="submit">Atualizar perfil</button>
             </Form>
-            <button type="button">Sair do GoBarber</button>
+            <button type="button" onClick={handleSignOut}>
+                Sair do GoBarber
+            </button>
         </Container>
     );
 }
