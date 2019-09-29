1df3740eca5f13b66f3e07ffffe2535deb28c4ca
Marlon <marlon.bobi@hotmail.com>
2019-09-28 21:48:30 -0300

    Modulo09-Estilização do Dashboard

--- bootcamp/modulo09/src/pages/Dashboard/index.js
+++ bootcamp/modulo09/src/pages/Dashboard/index.js
@@ -1,10 +1,41 @@
 import React from 'react';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 
 import api from '~/services/api';
 
-// import { Container } from './styles';
+import { Container, Time } from './styles';
 
 export default function Dashboard() {
-    api.get('appointments');
-    return <h1>Dashboard</h1>;
+    return (
+        <Container>
+            <header>
+                <button type="button">
+                    <MdChevronLeft size={36} color="#FFF" />
+                </button>
+                <stron>31 de Maio</stron>
+                <button type="button">
+                    <MdChevronRight size={36} color="#FFF" />
+                </button>
+            </header>
+
+            <ul>
+                <Time past>
+                    <strong>21:00</strong>
+                    <span> Marlon Souza</span>
+                </Time>
+                <Time available>
+                    <strong>22:00</strong>
+                    <span>Em aberto</span>
+                </Time>
+                <Time>
+                    <strong>23:00</strong>
+                    <span> Marlon Souza</span>
+                </Time>
+                <Time>
+                    <strong>00:00</strong>
+                    <span> Marlon Souza</span>
+                </Time>
+            </ul>
+        </Container>
+    );
 }
