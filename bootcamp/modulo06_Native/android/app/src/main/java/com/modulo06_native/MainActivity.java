7e88291daceca42fd5841fcdfac01619516b69af
Marlon <marlon.bobi@hotmail.com>
2019-10-04 21:07:47 -0300

    Modulo06N-React Navigation

--- bootcamp/modulo06_Native/android/app/src/main/java/com/modulo06_native/MainActivity.java
+++ bootcamp/modulo06_Native/android/app/src/main/java/com/modulo06_native/MainActivity.java
@@ -1,6 +1,9 @@
 package com.modulo06_native;
 
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends ReactActivity {
 
@@ -12,4 +15,14 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "modulo06_Native";
   }
+
+@Override
+protected ReactActivityDelegate createReactActivityDelegate() {
+return new ReactActivityDelegate(this, getMainComponentName()) {
+@Override
+protected ReactRootView createRootView() {
+return new RNGestureHandlerEnabledRootView(MainActivity.this);
+}
+};
+}
 }
--- bootcamp/modulo06_Native/src/index.js
+++ bootcamp/modulo06_Native/src/index.js
@@ -1,31 +1,10 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+// import { View } from 'react-native';
 
 import './config/ReactotronConfig';
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-});
-
-console.tron.log('Hi');
+import Routes from './routes';
 
 export default function App() {
-  return (
-    <View style={styles.sectionContainer}>
-      <Text style={styles.sectionTitle}>App test2</Text>
-      <Text style={styles.sectionDescription}>Welcome to my app!</Text>
-    </View>
-  );
+  return <Routes />;
 }
