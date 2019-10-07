9528349ce4d53d2b5d4ad579fba8fb85bf4c7f59
Marlon <marlon.bobi@hotmail.com>
2019-10-07 17:56:20 -0300

    Modulo06N-Salvando no storage

--- bootcamp/modulo06_Native/src/config/ReactotronConfig.js
+++ bootcamp/modulo06_Native/src/config/ReactotronConfig.js
@@ -1,8 +1,8 @@
 import Reactotron from 'reactotron-react-native';
 
 if (__DEV__) {
-  const tron = Reactotron.configure({ host: '7.212.4.61' })
-    // const tron = Reactotron.configure({ host: '192.168.25.32' })
+  // const tron = Reactotron.configure({ host: '7.212.4.61' })
+  const tron = Reactotron.configure({ host: '192.168.25.32' })
     .useReactNative()
     .connect();
 
