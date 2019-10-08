03214d35544d8aac557281cdf0f41f39423eb078
Marlon Souza <42625569+souzamarlon@users.noreply.github.com>
2019-10-08 08:56:11 -0300

    Modulo06N-Listando favoritos

--- bootcamp/modulo06_Native/src/pages/User/index.js
+++ bootcamp/modulo06_Native/src/pages/User/index.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
 
 import PropTypes from 'prop-types';
 
 import api from '../../services/api';
+import {
+  Container,
+  Header,
+  Avatar,
+  Name,
+  Bio,
+  Stars,
+  Starred,
+  OwnerAvatar,
+  Info,
+  Title,
+  Author,
+} from './styles';
 
 // import { Container } from './styles';
 
@@ -32,7 +44,31 @@ export default class User extends Component {
   }
 
   render() {
+    const { navigation } = this.props;
     const { stars } = this.state;
-    return <View />;
+
+    const user = navigation.getParam('user');
+    return (
+      <Container>
+        <Header>
+          <Avatar source={{ uri: user.avatar }} />
+          <Name>{user.name}</Name>
+          <Bio>{user.bio}</Bio>
+        </Header>
+        <Stars
+          data={stars}
+          keyExtractor={star => String(star.id)}
+          renderItem={({ item }) => (
+            <Starred>
+              <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
+              <Info>
+                <Title>{item.name}</Title>
+                <Author>{item.owner.login}</Author>
+              </Info>
+            </Starred>
+          )}
+        />
+      </Container>
+    );
   }
 }
