4fc800758db374fb1ca36078f2d207fe2fa8b32a
Marlon Souza <42625569+souzamarlon@users.noreply.github.com>
2019-10-10 16:00:00 -0300

    Modulo09N-Página SignIn_Part1

--- bootcamp/modulo09_Native/src/pages/SignIn/index.js
+++ bootcamp/modulo09_Native/src/pages/SignIn/index.js
@@ -1,24 +1,37 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Image } from 'react-native';
 
-import Input from '~/components/Input';
+import logo from '~/assets/logo.png';
+
 import Button from '~/components/Button';
 import Background from '~/components/Background';
 
-// import { Container } from './styles';
+import { Container, Form, FormInput, SubmitButton } from './styles';
 
 export default function SignIn() {
   return (
     <Background>
-      <Text>SignIn</Text>
+      <Container>
+        <Image source={logo} />
 
-      <Input
-        style={{ marginTop: 30 }}
-        icon="call"
-        placeholder="Digite seu nome"
-      />
+        <Form>
+          <FormInput
+            icon="main-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu e-mail"
+          />
 
-      <Button>Entrar</Button>
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Sua senha secreta"
+          />
+
+          <SubmitButton
+        </Form>
+      </Container>
     </Background>
   );
 }
