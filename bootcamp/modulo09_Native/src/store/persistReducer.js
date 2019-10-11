599ac35e6e8a64e26bf1822215828df743bccb9a
Marlon <marlon.bobi@hotmail.com>
2019-10-11 18:30:53 -0300

    Modulo09N-Configurando Redux-Its working!

--- bootcamp/modulo09_Native/src/store/persistReducer.js
+++ bootcamp/modulo09_Native/src/store/persistReducer.js
@@ -1,11 +1,14 @@
-import storage from 'redux-persist/lib/storage';
+// import storage from 'redux-persist/lib/storage';
+
+import AsyncStorage from '@react-native-community/async-storage';
+
 import { persistReducer } from 'redux-persist';
 
 export default reducers => {
   const persistedReducer = persistReducer(
     {
       key: 'gobarber',
-      storage,
+      storage: AsyncStorage,
       whitelist: ['auth', 'user'],
     },
     reducers
