9c96d489f2aa30ebec2400476f6518ba95f66184
Marlon Souza <42625569+souzamarlon@users.noreply.github.com>
2019-11-04 08:12:27 -0300

    Modulo09-Desafio-Details-Delete button is working!

--- bootcamp/modulo09_desafio_react/src/components/Header/index.js
+++ bootcamp/modulo09_desafio_react/src/components/Header/index.js
@@ -2,16 +2,18 @@ import React from 'react';
 
 import { Link } from 'react-router-dom';
 import { Container } from './styles';
-import logo from '~/assets/logo.svg';
+import logo from '~/assets/logo_dash.svg';
 
 export default function Header() {
     return (
         <Container>
-            <nav>
-                <Link to="/dashboard">
-                    <img src={logo} alt="Meetup" />
-                </Link>
-            </nav>
+            <>
+                <nav>
+                    <Link to="/dashboard">
+                        <img src={logo} alt="Meetup" />
+                    </Link>
+                </nav>
+            </>
         </Container>
     );
 }
--- bootcamp/modulo09_desafio_react/src/components/Header/styles.js
+++ bootcamp/modulo09_desafio_react/src/components/Header/styles.js
@@ -2,25 +2,33 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
     background: #000000;
+    /* margin: 0 0 0 500px; */
+    left: 0px;
+    /* width: 1440px; */
     padding: 0 30px;
     height: 64px;
     display: flex;
     align-items: center;
+    align-self: center;
 
-    /* mix-blend-mode: normal; */
+    mix-blend-mode: normal;
     opacity: 0.3;
 
     nav {
         display: flex;
         align-items: center;
+
         img {
-            margin-right: 20px;
             padding-right: 20px;
-            border-right: 1px solid #eee;
+            border: 0;
+            width: 60px;
+            height: 62px;
+            color: #f94d6a;
+            /* border-right: 1px solid #eee; */
         }
-        a {
+        /* a {
             font-weight: bold;
             color: #7159c1;
-        }
+        } */
     }
 `;
