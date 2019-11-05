520e716715298e875348a4be19341cae47b929cd
Marlon <marlon.bobi@hotmail.com>
2019-11-04 21:53:11 -0300

    Modulo09-Desafio-The profile page is done!

--- bootcamp/modulo09_desafio_react/src/routes/index.js
+++ bootcamp/modulo09_desafio_react/src/routes/index.js
@@ -8,6 +8,7 @@ import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 import Dashboard from '../pages/Dashboard';
 import NewMeetup from '../pages/NewMeetup';
+import Profile from '../pages/Profile';
 
 import Detail from '../pages/Detail';
 
@@ -19,6 +20,7 @@ export default function Routes() {
             <Route path="/dashboard" component={Dashboard} isPrivate />
             <Route path="/detail/:id" component={Detail} isPrivate />
             <Route path="/newmeetup" component={NewMeetup} isPrivate />
+            <Route path="/profile" component={Profile} isPrivate />
         </Switch>
     );
 }
