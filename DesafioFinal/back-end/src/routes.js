96a7e18e3c1ba117a79fd82f58cd77a58c05999b
Marlon Souza <42625569+souzamarlon@users.noreply.github.com>
2019-11-12 08:03:13 -0300

    Desafio Final-back-end Finished to setup the project, I can start my back end

---
+++ DesafioFinal/back-end/src/routes.js
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+// import multer from 'multer';
+// import multerConfig from './config/multer';
+// import UserController from './app/controllers/UserController';
+
+// import authMiddleware from './app/middlewares/auth';
+
+const routes = new Router();
+// const upload = multer(multerConfig);
+
+// routes.post('/users', UserController.store);
+// routes.get('/users', UserController.index);
+
+// routes.use(authMiddleware);
+
+export default routes;
