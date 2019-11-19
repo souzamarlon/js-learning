b12066814493b8a12485405af6fe896c685cc628
Marlon <marlon.bobi@hotmail.com>
2019-11-18 22:29:49 -0300

    Desafio final-Back End, Created the checkins controller

---
+++ DesafioFinal/back-end/src/app/controllers/CheckinController.js
@@ -0,0 +1,18 @@
+import * as Yup from 'yup';
+
+import Checkin from '../models/Checkin';
+
+class CheckinController {
+  // async index(req, res) {
+  //   const name = await User.findAll();
+  //   return res.json(name);
+  // }
+
+  async store(req, res) {}
+
+  // TODO - I still need to improve
+
+  // async update(req, res) {}
+}
+
+export default new CheckinController();
