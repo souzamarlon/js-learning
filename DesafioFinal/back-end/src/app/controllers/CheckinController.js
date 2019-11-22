399fbec146b7461ad7993750da3ce6bcb01e9554
Marlon <marlon.bobi@hotmail.com>
2019-11-21 21:41:25 -0300

    Desafio final-Back End, The checkin is working and the validation too!

--- DesafioFinal/back-end/src/app/controllers/CheckinController.js
+++ DesafioFinal/back-end/src/app/controllers/CheckinController.js
@@ -1,6 +1,6 @@
 import * as Yup from 'yup';
 
-import { startOfDay, endOfDay, parseISO, format, subDays } from 'date-fns';
+import { startOfDay, endOfDay, subDays } from 'date-fns';
 import { Op } from 'sequelize';
 
 import Checkin from '../models/Checkin';
@@ -18,39 +18,43 @@ class CheckinController {
   async store(req, res) {
     const { id } = req.params;
 
+    // TODO
+    // Reduzir a data para 7 dias e começar a contagem a partir desssa data e fazer um contador
+    // Para validar se ele já tem 5 checkins nos ultimos 7 dias.
+
     // Actual date format(value, "yyyy-MM-dd'T'HH:mm:ssxxx")
-    const date = parseISO(2019 - 11 - 21);
     const actualDate = new Date();
 
     // Reducing 7 days from actual date
     // const less7days = subDays(actualDate, 7);
     const less7days = subDays(actualDate, 7);
 
-    console.log(date);
+    console.log(actualDate);
     console.log(less7days);
 
     const createdAt = await Checkin.findAll({
       where: {
         student_id: id,
         createdAt: {
-          [Op.between]: [startOfDay(date), endOfDay(actualDate)],
+          [Op.between]: [startOfDay(less7days), endOfDay(actualDate)],
         },
       },
-      // attributes: ['id', 'createdAt'],
+      attributes: ['id', 'createdAt'],
     });
 
-    // TODO
-    // Reduzir a data para 7 dias e começar a contagem a partir desssa data e fazer um contador
-    // Para validar se ele já tem 5 checkins nos ultimos 7 dias.
-    console.log(createdAt);
+    console.log(createdAt.length);
 
-    // console.log(createdAt.length);
+    if (createdAt.length >= 5) {
+      return res
+        .status(400)
+        .json({ error: 'Você já realizou 5 checkins em um período de 7 dias' });
+    }
 
-    // const checkins = await Checkin.create({
-    //   student_id: id,
-    // });
+    const checkins = await Checkin.create({
+      student_id: id,
+    });
 
-    return res.json(createdAt);
+    return res.json(checkins);
   }
 
   // TODO - I still need to improve
