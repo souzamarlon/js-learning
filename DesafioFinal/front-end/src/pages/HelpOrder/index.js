ade430dc2a8e36830e2b3d707ee25b6f00f64281
Marlon <marlon.bobi@hotmail.com>
2020-01-11 22:02:07 -0300

    Desafio final-Front-End, Help order page is done but I still need to do the answer page

--- DesafioFinal/front-end/src/pages/HelpOrder/index.js
+++ DesafioFinal/front-end/src/pages/HelpOrder/index.js
@@ -1,7 +1,59 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-// import { Container } from './styles';
+import { Link } from 'react-router-dom';
+
+import { Container, Title, Content } from './styles';
+import api from '~/services/api';
+import history from '~/services/history';
 
 export default function HelpOrder() {
-    return <div />;
+    const [help, setHelp] = useState([]);
+
+    useEffect(() => {
+        async function listHelpOrders() {
+            const response = await api.get('students/help-orders');
+
+            setHelp(response.data);
+        }
+
+        listHelpOrders();
+    }, []);
+
+    console.tron.log(help);
+
+    return (
+        <>
+            <Container>
+                <Title>
+                    <h1>Pedidos de auxílio</h1>
+                </Title>
+            </Container>
+            <Content>
+                <thead>
+                    <tr>
+                        <th>ALUNO</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {help.map(item => (
+                        <tr>
+                            <td>
+                                <span className="name">
+                                    {item.student.name}
+                                </span>
+                            </td>
+                            <td>
+                                <button type="button" onClick={() => {}}>
+                                    <Link to={`/editplan/${item.id}`}>
+                                        responder
+                                    </Link>
+                                </button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </Content>
+        </>
+    );
 }
