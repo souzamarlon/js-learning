a7b4fa9d23616c9bb189cb255e5db63d3cdfd8f2
Marlon <marlon.bobi@hotmail.com>
2020-01-19 21:53:32 -0300

    Desafio final Mobile, Doing the Dashboard page!

--- DesafioFinal_Gympoint/mobile/src/pages/Dashboard/index.js
+++ DesafioFinal_Gympoint/mobile/src/pages/Dashboard/index.js
@@ -2,16 +2,20 @@ import React from 'react';
 import { View } from 'react-native';
 import { useSelector } from 'react-redux';
 
+import Background from '~/components/Background';
+
 // import { Container } from './styles';
 
 import api from '~/services/api';
 
 export default function Dashboard() {
-  // const id = useSelector(state => state.auth.signed);
+  const id = useSelector(state => state.auth.id);
+
+  console.tron.log(id);
 
   // async function loadCheckins() {
   //   const response = await api.get(`/students/${id}/checkins`);
   // }
 
-  return <View />;
+  return <Background />;
 }
--- DesafioFinal_Gympoint/mobile/src/routes.js
+++ DesafioFinal_Gympoint/mobile/src/routes.js
@@ -1,16 +1,53 @@
 // import React from 'react';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 import SignIn from './pages/SignIn';
+import Dashboard from './pages/Dashboard';
 
 // export default createAppContainer(createSwitchNavigator({ SignIn }));
 
 export default (isSigned = false) =>
+  // createAppContainer(
+  //   createSwitchNavigator({
+  //     SignIn,
+  //     // Sign: createSwitchNavigator({
+  //     //   ,
+  //     // }),
+  //     App: createBottomTabNavigator({
+  //       Dashboard,
+  //       {
+  //         initialRouteName: isSigned ? 'App' : 'SignIn',
+  //       }
+  //     }),
+
+  //   })
+  // );
+
   createAppContainer(
-    createSwitchNavigator({
-      SignIn,
-      // Sign: createSwitchNavigator({
-      //   ,
-      // }),
-      initialRouteName: isSigned ? 'SignIn' : 'SignIn',
-    })
+    createSwitchNavigator(
+      {
+        Sign: createSwitchNavigator({
+          SignIn,
+        }),
+        App: createBottomTabNavigator(
+          {
+            Dashboard,
+          },
+          {
+            resetOnBlur: true,
+            tabBarOptions: {
+              keyboardHidesTabBar: true,
+              activeTintColor: '#FFF',
+              inactiveTintColor: 'rgba(255,255, 255, 0.6)',
+              style: {
+                backgroundColor: '#FFFF',
+              },
+            },
+          }
+        ),
+      },
+      {
+        initialRouteName: isSigned ? 'App' : 'Sign',
+      }
+    )
   );
