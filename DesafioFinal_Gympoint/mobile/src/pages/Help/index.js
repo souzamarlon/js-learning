78d580608ec0f49000331ab1a2666125be113e95
Marlon <marlon.bobi@hotmail.com>
2020-01-26 22:05:35 -0300

    Desafio final Mobile, Doing the help pae!

--- DesafioFinal_Gympoint/mobile/src/pages/Help/index.js
+++ DesafioFinal_Gympoint/mobile/src/pages/Help/index.js
@@ -1,8 +1,57 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useEffect, useState } from 'react';
 
-// import { Container } from './styles';
+import { useSelector } from 'react-redux';
+import { Alert } from 'react-native';
+
+import Checkin from '~/components/Checkin';
+
+import Header from '~/components/Header';
+
+import { Container, SubmitButton, List } from './styles';
+
+import api from '~/services/api';
 
 export default function Help() {
-  return <View />;
+  const id = useSelector(state => state.auth.id);
+  const [checkin, setCheckin] = useState();
+
+  const [refreshList, setRefreshList] = useState(false);
+
+  useEffect(() => {
+    async function loadCheckins() {
+      const response = await api.get(`/students/${id}/checkins`);
+
+      const checkins = response.data.map(item => ({
+        ...item,
+        index: response.data.indexOf(item) + 1,
+      }));
+
+      setCheckin(checkins.sort((a, b) => a.createdAt < b.createdAt));
+      console.tron.log(checkins);
+      // setCheckin(checkins);
+      setRefreshList(false);
+    }
+    loadCheckins();
+  }, [id, refreshList]);
+
+  async function loadPage() {
+    setRefreshList(true);
+  }
+
+  return (
+    <>
+      <Header />
+      <Container>
+        <SubmitButton onPress={() => {}}>Novo check-in</SubmitButton>
+
+        <List
+          refreshing={refreshList}
+          onRefresh={loadPage}
+          data={checkin}
+          keyExtractor={item => String(item.id)}
+          renderItem={({ item }) => <Checkin onCancel={() => {}} data={item} />}
+        />
+      </Container>
+    </>
+  );
 }
--- DesafioFinal_Gympoint/mobile/src/pages/Help/styles.js
+++ DesafioFinal_Gympoint/mobile/src/pages/Help/styles.js
@@ -1,3 +1,20 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+import Button from '~/components/Button';
+
+export const Container = styled.View`
+  flex: 1;
+  background: transparent;
+`;
+
+export const SubmitButton = styled(Button)`
+  /* width: 200px; */
+
+  margin: 0 30px;
+  margin-top: 5px;
+`;
+
+export const List = styled.FlatList.attrs({
+  showsVerticalScrollIndicator: false,
+  contentContainerStyle: { padding: 30 },
+})``;
