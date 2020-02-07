26e468846b7037cdfd47a50da983b894398b6d17
Marlon <marlon.bobi@hotmail.com>
2020-02-06 22:56:13 -0300

    Desafio final Mobile, Finished the NewHelpOrder page layout! I need to do the submitbutton works

--- DesafioFinal_Gympoint/mobile/src/pages/Help/NewOrder/index.js
+++ DesafioFinal_Gympoint/mobile/src/pages/Help/NewOrder/index.js
@@ -1,8 +1,55 @@
 import React from 'react';
-import { View } from 'react-native';
 
-// import { Container } from './styles';
+import { useSelector } from 'react-redux';
+
+import { Alert, ScrollView } from 'react-native';
+import { Container, Form, FormInput, SubmitButton } from './styles';
+import api from '~/services/api';
+
+import Header from '~/components/Header';
 
 export default function NewOrder() {
-  return <View />;
+  const id = useSelector(state => state.auth.id);
+  async function handleAddCheckin() {
+    try {
+      const response = await api.post(`students/${id}/checkins`);
+      Alert.alert('Sucesso!', 'Check-in realizado com sucesso!');
+
+      console.tron.log(response.data);
+    } catch (err) {
+      Alert.alert('Falha!', 'Você já realizou 5 checkins nos ultimos 7 dias.');
+      console.tron.log(err);
+    }
+  }
+
+  return (
+    <>
+      <Header />
+      <ScrollView
+        contentContainerStyle={{ flexGrow: 1 }}
+        keyboardShouldPersistTaps="handled"
+      >
+        <Container>
+          <Form>
+            <FormInput
+              keyboardType="default"
+              autoCorrect={false}
+              autoCapitalize="none"
+              placeholder="Inclua seu pedido de auxílio"
+              returnKeyType="next"
+              multiline
+              // numberOfLines={100}
+
+              // onSubmitEditing={() => emailRef.current.focus()}
+              // value={name}
+              // onChangeText={setName}
+            />
+            <SubmitButton onPress={handleAddCheckin}>
+              Novo check-in
+            </SubmitButton>
+          </Form>
+        </Container>
+      </ScrollView>
+    </>
+  );
 }
--- DesafioFinal_Gympoint/mobile/src/pages/Help/NewOrder/styles.js
+++ DesafioFinal_Gympoint/mobile/src/pages/Help/NewOrder/styles.js
@@ -1,3 +1,34 @@
 import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
+import Button from '~/components/Button';
+import Input from '~/components/Input';
 
-export const Container = styled.View``;
+export const Container = styled(RectButton)`
+  /* margin-top: 2px; */
+  height: 350px;
+  /* width: 100%; */
+  max-width: 335px;
+  /* padding-top: 10px; */
+  border-radius: 4px;
+  /* background: #ffff; */
+  /* border: 1px solid #eee; */
+  margin: 20px;
+`;
+export const Form = styled.View`
+  align-self: stretch;
+  /* margin-top: 50px; */
+`;
+
+export const FormInput = styled(Input)`
+  margin-bottom: 20px;
+  background: #fff;
+  height: 300px;
+  align-items: flex-start;
+  padding-top: 15px;
+`;
+
+export const SubmitButton = styled(Button)`
+  max-width: 335px;
+  /* margin: 0 0px; */
+  margin-top: 10px;
+`;
